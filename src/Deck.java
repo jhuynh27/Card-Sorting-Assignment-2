dc68b72cc7a3baaf049380c748e072d33e2113c3
Justin Huynh <justin@Justins-MacBook-Air-2.local>
2025-10-24 10:03:44 -0500

    deck constructors & toString and shuffle methods

--- src/Deck.java
+++ src/Deck.java
@@ -13,7 +13,58 @@ public class Deck {
    private int topCard;
    
    public Deck() {
+      cards = new Card[52];
+      topCard = 51;
+      
+      int count = 0;
+      for (int s = 0; s <= 3; s++) {
+         for (int r = 1; r <= 13; r++) {
+            cards[count] = new Card(s, r);
+            count++;
+         }
+      }
+      
+   }
+   
+   public Deck(boolean isSorted) {
+      if (isSorted == true) {
+         cards = new Card[52];
+         topCard = 51;
+      
+         int count = 0;
+         for (int s = 0; s <= 3; s++) {
+            for (int r = 1; r <= 13; r++) {
+               deck[count] = new Card(s, r);
+               count++;
+            }
+         }
+      }
+      else { 
+         cards = new Card[52];
+         topCard = 51;
+      
+         int count = 0;
+         for (int s = 0; s <= 3; s++) {
+            for (int r = 1; r <= 13; r++) {
+               deck[count] = new Card(s, r);
+               count++;
+            }
+         }
+         cards.shuffle();
+      }
    
    }
    
+   public Deck(Deck other) {
+      this.topCard = other.topCard;
+      this.cards = new Card(other.cards.length);
+      for (int i = 0; i <= topCard; i++) {
+         this.cards[i] = new Card(other.cards[i]);
+      }
+   }
+   
+   @Override
+   public String toString() {
+      
+   } 
 }
