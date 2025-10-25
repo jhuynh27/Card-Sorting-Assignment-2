892e18bfb039f0d6ca6a222458191ce0faa5540f
Justin Huynh <justin@Justins-MacBook-Air-2.local>
2025-10-24 23:11:14 -0600

    CardTest and DeckTest

--- src/CardTest.java
+++ src/CardTest.java
@@ -2,25 +2,69 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class CardTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+public class CardTest {
+    
     @Test
     public void testDefaultConstructor() {
-        Card card = new Card();
-        assertNotNull(card);
+      Card card = new Card();
+      assertNotNull(card);
     }
-
+    
+    @Test
+    public void testStringtoStringConstructor() {
+      Card card = new Card("Spades", "Two");
+      assertEquals("Spades", card.getSuitInt());
+      assertEquals(3, card.getRank());
+    }
+    
+    @Test
+    
+    public void testIntToIntConstructor() {
+      Card card = new Card(2, 12);
+      assertEquals("Hearts", card.getSuit());
+      assertEquals(12, card.getRank());
+    }
+    
+    @test
+    public void testIntToStringConstructor() {
+      Card card = new Card(1, "Jack");
+      assertEquals("Diamonds", card.getSuit());
+      assertEquals("Jack", card.getRank());
+    }
+    
+    @Test
+    public void testStringToIntConstructor() {
+      Card card = new Card("Clubs", 4);
+      assertEquals("Clubs", card.getSuit());
+      assertEquals(4, card.getRank());
+    }
+    
+    @Test
+    public void testCopyConstructor() {
+      Card original = new Card(3, 9);
+      Card copy = new Card(original);
+      assertEquals(original, copy);
+      assertNotSame(original, copy);
+    }
+    
     @Test
     public void testToString() {
-        Card card = new Card("Hearts", "King");
-        assertEquals("King of Hearts", card.toString());
+      Card card = new Card("Hearts", "King");
+      assertEquals("King of Hearts", card.toString());
     }
-
+    
+    @Test
+    public void testGetStringSuit() {
+      Card card = new Card("Clubs", "Ace");
+      assertEquals("Clubs", card.getSuit());
+    }
+    
+    @Test
+    public void testGetIntRank() {
+      Card card = new Card("Spades", "Ace");
+      assertEquals(1, card.getRank());
+    }
+      
     @Test
     public void testGetSuitInt() {
         Card card = new Card("Clubs", 3);
@@ -32,6 +76,39 @@ class CardTest {
         Card card = new Card("Diamonds", 1); 
         assertEquals("Ace", card.getRankStr());
     }
-
+    
+    @Test
+    public void testCompareToSameCard() {
+      Card card1 = new Card("Hearts", "Queen");
+      Card card2 = new Card("Hearts", "Queen");
+      assertEquals(0, card1.compareTo(card2));
+    }
+    
+    @Test
+    public void testCompareToDifferentSuits() {
+      Card card1 = new Card("Hearts", "Queen");
+      Card card2 = new Card("Spades", "Queen");
+      assertEquals(card1.compareTo(card2) < 0);
+    }
+    
+    @Test
+    public void testCompareToDifferentRanks() {
+      Card card1 = new Card("Spades", "Six");
+      Card card2 = new Card("Spades", "Seven");
+      assertEquals(card1.compareTo(card2) < 0);
+    }
+    
+    @Test
+    public void testEquals() {
+      Card card1 = new Card("Clubs", "Ten");
+      Card card2 = new Card("Clubs", "Ten");
+      assertEquals(card1, card2);
+    }
+    
+    @Test
+    public void testNotEquals() {
+      Card card1 = new Card("Clubs", "Ten");
+      Card card2 = new Card("Hearts", "Ten");
+      assertNotEquals(card1, card2);
+    }
 }
-
--- src/DeckTest.java
+++ src/DeckTest.java
@@ -2,19 +2,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class DeckTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+public class DeckTest {
+      
 	@Test
 	public void testDefaultDeckConstructor() {
 		Deck deck = new Deck();
 		assertEquals(51, deck.getTopCard());
 	}
-
+   
 	@Test
 	public void testCopyConstructor() {
 		Deck original = new Deck();
@@ -32,5 +27,45 @@ class DeckTest {
 			assertEquals(4, hand.getTopCard()); 
 		}
 	}
-
+   
+   @Test
+   public void testShufffle() {
+      Deck deck1 = new Deck(true);
+      Deck deck2 = new Deck(true);
+      deck2.shuffle();
+      assertNotEquals(deck1, deck2);
+   }
+      
+   @Test
+   public void testEquals() {
+      Deck deck1 = new Deck();
+      Deck deck2 = new Deck(deck1);
+      assertEquals(deck1, deck2);
+   }
+   
+   @Test
+   public void testPick() {
+      Deck deck = new Deck();
+      int a = deck.getTopCard();
+      Card card = deck.pick();
+      int b = deck.getTopCard();
+      assertNotEquals(a, b);
+   }
+   
+   @Test
+   public void testSelectionSort() {
+      Deck deck = new Deck(false);
+      Deck deck2 = new Deck(true);
+      deck.selectionSort();
+      assertEquals(deck, deck2);
+   }
+   
+   @Test
+   public void testMergeSort() {
+      Deck deck = new Deck(false);
+      Deck deck2 = new Deck(true);
+      deck.mergeSort();
+      assertEquals(deck, deck2);
+   }
+   
 }
